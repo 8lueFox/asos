62b2ab3c4deeb971831782220b6a70e6f2b33153
Kacper <kacper.jedrzejewski@int.pl>
2020-01-28 17:17:07 +0100

    [init]

---
+++ src/main/java/com/io/usos/UsosApplication.java
@@ -0,0 +1,13 @@
+package com.io.usos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/io/usos/UsosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.usos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
